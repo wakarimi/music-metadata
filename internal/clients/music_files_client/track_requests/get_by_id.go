0869713aea5a5a801242ab3320c8e195a5cbde55
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-09-18 17:17:35 +0300

    feat: Update track request by id

--- internal/clients/music_files_client/track_requests/get_by_id.go
+++ internal/clients/music_files_client/track_requests/get_by_id.go
@@ -10,14 +10,20 @@ import (
 )
 
 type GetByIdResponse struct {
-	TrackId   int    `json:"trackId"`
-	CoverId   *int   `json:"coverId"`
-	Extension string `json:"extension"`
-	Size      int    `json:"size"`
-	Hash      string `json:"hash"`
+	TrackId      int    `json:"trackId"`
+	CoverId      *int   `json:"coverId,omitempty"`
+	DurationMs   int64  `json:"durationMs"`
+	SizeByte     int64  `json:"sizeByte"`
+	AudioCodec   string `json:"audioCodec"`
+	BitrateKbps  int    `json:"bitrateKbps"`
+	SampleRateHz int    `json:"sampleRateHz"`
+	Channels     int    `json:"channels"`
+	HashSha256   string `json:"hashSha256"`
 }
 
 func (c *TrackClient) GetById(trackId int) (track GetByIdResponse, err error) {
+	log.Debug().Int("trackId", trackId).Msg("Fetching track by id from music-files service")
+
 	resp, err := c.musicFilesClient.Request(http.MethodGet, "/api/music-files-service/tracks/"+strconv.Itoa(trackId), nil)
 	if err != nil {
 		log.Error().Err(err).Int("trackId", trackId).Msg("Failed to execute request for getting track by ID")
@@ -43,5 +49,6 @@ func (c *TrackClient) GetById(trackId int) (track GetByIdResponse, err error) {
 		return GetByIdResponse{}, err
 	}
 
+	log.Debug().Int("trackId", track.TrackId).Str("sha256", track.HashSha256).Msg("Track fetched successfully")
 	return track, nil
 }
